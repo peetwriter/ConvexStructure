8304939bb9c7396f8a1f91b671ebb803b56d788f
peetwriter <pereter92@gmail.com>
2014-02-26 23:02:21 +0100

    maximum distance for vector of points trial function added

--- ConvexStruct/main.cpp
+++ ConvexStruct/main.cpp
@@ -10,31 +10,36 @@ inline bool operator == (const std::string &st, const::string &srt)
     return (true);
 }
 
-void incr_p(int* p){++*p;}
-
-struct X {
-    int val;
-    void out(const string& s)
-    { cerr << this << "->" << s << ": " << val << "\n "; }
-    X(){ out("X()"); val=0; }
-    X(int v) { out( "X(int)"); val=v; }
-    X(const X& x){ out("X(X&) "); val=x.val; }
-    X& operator=(const X& a)
-    { out("X: :operator=()"); val=a.val; return *this; }
-    ~X() { out("-X()"); }
-};
-
-X glob(2);
-X copy(X a) { return a; }
-X copy2(X a) { X aa = a; return aa; }
-X& ref_to(X& a) { return a; }
-X* make(int i) { X a(i); return new X(a); }
-struct XX { X a; X b; };
-
-
+vector<Point> maxDist(vector<Point> inPoints )
+{
+    double maxDist = 0;
+    vector<Point> maxDistPoints(2);
+    for(int i=0; i< inPoints.size(); i++)
+    {
+        for(int j=0; j< inPoints.size(); j++)
+        {
+            if (inPoints[i].dist(inPoints[j]) > maxDist)
+            {
+                maxDist = inPoints[i].dist(inPoints[j]);
+                cout<< maxDist << endl;
+                maxDistPoints[0] = inPoints[i];
+                maxDistPoints[1] = inPoints[j];
+            }
+        }
+    }
+    return maxDistPoints;
+}
 
 int main()
 {
-
+   vector<Point> points;
+   Point* point1  = new Point(1.0, 1.0, 1.0);
+   Point* point2  = new Point(2.0, 2.0, 2.0);
+   Point* point3  = new Point(1.0, 2.0, 3.0);
+   points.push_back(*point1);
+   points.push_back(*point2);
+   points.push_back(*point3);
+   vector<Point> maxPoints = maxDist(points);
+   cout<< maxPoints[1].x()<< " "<< maxPoints[1].y()<< endl;
 }
 
--- ConvexStruct/point.hh
+++ ConvexStruct/point.hh
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <math.h>
+#include <vector>
+
 
 using namespace std;
 
